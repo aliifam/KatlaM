8bdf729d7c1289292ac7ccaa4b77e803fb941787
aliifam <aliif.arief.m@gmail.com>
2023-11-15 20:16:45 +0700

    feat: game board styling

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import {
     StatusBar,
     useColorScheme,
     StyleSheet,
+    ScrollView,
+    View,
 } from 'react-native';
 import React from 'react';
 import {colors} from './src/constants/constants';
@@ -11,6 +13,13 @@ import Keyboard from './src/components/keyboard';
 
 const App = () => {
     const isDarkMode = useColorScheme() === 'dark';
+
+    const kata = 'hallo';
+    const huruf = kata.split('');
+    const maxtry = 6;
+
+    const rows = new Array(maxtry).fill(new Array(huruf.length).fill('a'));
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar
@@ -18,6 +27,19 @@ const App = () => {
                 backgroundColor={isDarkMode ? colors.black : colors.lightgrey}
             />
             <Text style={styles.title}>KATLAM</Text>
+            <ScrollView style={styles.map}>
+                {rows.map(row => (
+                    <View style={styles.row}>
+                        {row.map(cell => (
+                            <View style={styles.cell}>
+                                <Text style={styles.cellText}>
+                                    {cell.toUpperCase()}
+                                </Text>
+                            </View>
+                        ))}
+                    </View>
+                ))}
+            </ScrollView>
             <Keyboard />
         </SafeAreaView>
     );
@@ -31,10 +53,34 @@ const styles = StyleSheet.create({
     },
     title: {
         color: colors.lightgrey,
-        marginTop: 10,
+        marginVertical: 10,
         fontSize: 32,
         fontWeight: 'bold',
         letterSpacing: 7,
+    },
+    map: {
+        alignSelf: 'stretch',
+        height: 100,
+    },
+    row: {
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    cell: {
+        borderWidth: 3,
+        borderColor: colors.darkgrey,
+        flex: 1,
+        maxWidth: 70,
+        aspectRatio: 1,
+        margin: 3,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cellText: {
+        color: colors.lightgrey,
+        fontWeight: 'bold',
+        fontSize: 28,
     },
 });
 
